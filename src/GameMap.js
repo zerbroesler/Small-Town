e10d5899a3f823d5fa6438b3fc0e2a7242695030
zerbroesler <zerbroesler@gmx.de>
2017-04-22 22:41:51 +0200

    Push mising GameMap

--- src/GameMap.js
+++ src/GameMap.js
@@ -2,65 +2,37 @@ function GameMap(game){
 	
 	
 	var map;
-	var layer1;
-	var layer2;
-	var layer3;
+	var mapLayer;
 
 	var marker;
 	var currentTile = 0;
-	var currentLayer;
 
 	var cursors;
 	var showLayersKey;
-	var layer1Key;
-	var layer2Key;
-	var layer3Key;
+	var mapLayerKey;
 	
 	this.init = function(){
 	    //  Creates a blank tilemap
-		map = game.add.tilemap();
+		map = game.add.tilemap('Level1');
 
 		//  Add a Tileset image to the map
-		map.addTilesetImage('ground_1x1');
+		map.addTilesetImage('ground_1x1','ground_1x1');
 
-		//  Creates a new blank layer and sets the map dimensions.
-		//  In this case the map is 40x30 tiles in size and the tiles are 32x32 pixels in size.
-		layer1 = map.create('level1', 40, 30, 32, 32);
-		layer1.scrollFactorX = 0.5;
-		layer1.scrollFactorY = 0.5;
-
+		mapLayer = map.createLayer('level');
 		//  Resize the world
-		layer1.resizeWorld();
-
-		layer2 = map.createBlankLayer('level2', 40, 30, 32, 32);
-		layer2.scrollFactorX = 0.8;
-		layer2.scrollFactorY = 0.8;
-
-		layer3 = map.createBlankLayer('level3', 40, 30, 32, 32);
-
-		currentLayer = layer3;
+		mapLayer.resizeWorld();
 
 		//  Create our tile selector at the top of the screen
 		this.createTileSelector();
 
-		game.input.addMoveCallback(updateMarker, this);
+		game.input.addMoveCallback(this.updateMarker, this);
 
 		cursors = game.input.keyboard.createCursorKeys();
 
 		showLayersKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-		layer1Key = game.input.keyboard.addKey(Phaser.Keyboard.ONE);
-		layer2Key = game.input.keyboard.addKey(Phaser.Keyboard.TWO);
-		layer3Key = game.input.keyboard.addKey(Phaser.Keyboard.THREE);		
+		mapLayerKey = game.input.keyboard.addKey(Phaser.Keyboard.ONE);
 		
-	    showLayersKey.onDown.add(changeLayer, this);
-		layer1Key.onDown.add(changeLayer, this);
-		layer2Key.onDown.add(changeLayer, this);
-		layer3Key.onDown.add(changeLayer, this);
-
-		console.log(layer1.index);
-		console.log(layer2.index);
-		console.log(layer3.index);
-
+		console.log(mapLayer.index);
 	}
 	
 	this.pickTile = function(sprite, pointer) {
@@ -69,13 +41,13 @@ function GameMap(game){
 	
 	this.updateMarker = function() {
 
-		marker.x = currentLayer.getTileX(game.input.activePointer.worldX) * 32;
-		marker.y = currentLayer.getTileY(game.input.activePointer.worldY) * 32;
+		marker.x = mapLayer.getTileX(game.input.activePointer.worldX) * 32;
+		marker.y = mapLayer.getTileY(game.input.activePointer.worldY) * 32;
 
 		if (game.input.mousePointer.isDown)
 		{
-			map.putTile(currentTile, currentLayer.getTileX(marker.x), currentLayer.getTileY(marker.y), currentLayer);
-			// map.fill(currentTile, currentLayer.getTileX(marker.x), currentLayer.getTileY(marker.y), 4, 4, currentLayer);
+			map.putTile(currentTile, mapLayer.getTileX(marker.x), mapLayer.getTileY(marker.y), mapLayer);
+			// map.fill(currentTile, mapLayer.getTileX(marker.x), mapLayer.getTileY(marker.y), 4, 4, mapLayer);
 		}
 
 	}
@@ -94,7 +66,7 @@ function GameMap(game){
 
 		var tileStrip = tileSelector.create(1, 1, 'ground_1x1');
 		tileStrip.inputEnabled = true;
-		tileStrip.events.onInputDown.add(pickTile, this);
+		tileStrip.events.onInputDown.add(this.pickTile, this);
 
 		tileSelector.fixedToCamera = true;
 
